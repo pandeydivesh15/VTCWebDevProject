ce5647d3a8571e2012714684c9b3b1362f77ba42
Divesh Pandey <divesh.pandey.cse15@iitbhu.ac.in>
2016-06-25 14:11:45 +0530

    Adds certain security(md5 encryption, strip_tags(), mysqli_real_escape_string()) when user tries to change password

--- passwdChangeScript.php
+++ passwdChangeScript.php
@@ -6,16 +6,20 @@
 		$sql="SELECT Password FROM persons WHERE Email="."'".$_SESSION['email']."'";
 		$data=mysqli_query($conn,$sql) or die();
 		$rowForCheck=mysqli_fetch_array($data, MYSQLI_NUM);
-		if ($_POST['oldPasswd']!=$rowForCheck[0]) 
+		if (md5($_POST['oldPasswd'])!=$rowForCheck[0]) //Checks against md5() hashed value 
 		{
 			$result="Password NOT changed.<br>Incorrect Old Password given.";
 		}
 		elseif ($_POST['newPasswd']!=$_POST['confirmNewPasswd']) {
 			$result="Password NOT changed.<br>New Password doesn't matches Confirm Password field's value.";
 		}
+		elseif (strip_tags($_POST['newPasswd'])!=$_POST['newPasswd']) {
+			$result="Password NOT changed.<br>Make sure that you dont insert HTML/PHP tags in your new password.";
+		}
 		else
 		{
-			$sql="UPDATE persons SET Password="."'".$_POST['newPasswd']."'"." WHERE Email="."'".$_SESSION['email']."'";
+			$tempPwd=md5(strip_tags(mysqli_real_escape_string($conn,$_POST['newPasswd'])));
+			$sql="UPDATE persons SET Password="."'".$tempPwd."'"." WHERE Email="."'".$_SESSION['email']."'";
 			if (mysqli_query($conn,$sql))
 			{
 				$result="Password SUCCESSFULLY changed.<br><a href='logoutScript.php'>Click Here to LOG OUT</a>";		
